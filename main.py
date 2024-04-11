b6f6fb1b058524c058f402e1a2582da588005328
pmarashi5 <pmarashi5@gmail.com>
2024-04-11 14:13:56 -0400

    cleaned up encode

--- main.py
+++ main.py
@@ -22,17 +22,18 @@ def main():  # Main function
 def encode(password_in):  # Encoder function
     password_int_list = [int(i) for i in password_in]
     for i in range(len(password_int_list)):
-        password_int_list[i] += 3
+        password_int_list[i] = (password_int_list[i] + 3) % 10
     password_out = ''
     for i in password_int_list:
         password_out += str(i)
     return password_out
 
+
 # Anushka Kapoor (decode function)
 def decode(encoded_password):
     password_out_list = [int(i) for i in encoded_password]
     for i in range(len(password_out_list)):
-        password_out_list[i] -= 3
+        password_out_list[i] = (password_out_list[i] - 3) % 10
     decode_password = ''
     for i in password_out_list:
         decode_password += str(i)
